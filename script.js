56ec88bd3ab3cacd85c7106743a7cbd5e54352eb
Hari Dulai <hdcodesdev@gmail.com>
2023-05-14 13:17:18 +0100

    game functio while loop validate options

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ console.log("Welcome to the Rock Paper Scissors Game!");
 // will return a random choice from the array ['rock', 'paper', 'scissors']
 // display option in console.log
 
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const computersChoice = choices[Math.floor(Math.random() * choices.length)];
-  return computersChoice;
-}
-
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 // function should take two parameters - the playerSelection and computerSelection
 // then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
@@ -44,5 +38,34 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-let playerSelection = "rock";
-console.log(playRound(playerSelection, getComputerChoice()));
+// let playerSelection = "rock";
+// console.log(playRound(playerSelection, getComputerChoice()));
+
+// function game() { playRound() } , play round 5 times
+// keep the score
+// report as winner or loser
+
+const choices = ["rock", "paper", "scissors"];
+let round = 0;
+
+function getComputerChoice() {
+  const computersChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computersChoice;
+}
+
+function game() {
+  while (round < 5) {
+    let playerSelection = prompt("Rock, Paper or Scissors?");
+    playerSelection = playerSelection.toLowerCase();
+
+    if (!choices.includes(playerSelection)) {
+      alert("Invalid option");
+      continue;
+    }
+
+    playRound(playerSelection, getComputerChoice());
+    round++;
+  }
+}
+
+game();
